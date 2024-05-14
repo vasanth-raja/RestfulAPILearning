4c19c159745a6ba50aae74f4e44d310ab12d7f68
vasanth-raja <vasathrajark@gmail.com>
2024-05-14 13:51:30 +0530

    user data to DB

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,34 +5,53 @@ const User=require("../models/user")
 const brcypt=require("bcrypt")
 
 router.post('/signup',(req,res,next)=>{
-    brcypt.hash(req.body.password,10,(err,hash)=>{
-        if(err){
-            return res.status(500).json({
-                error:err
+    User.find({email:req.body.email}).exec().then(user=>{
+        if(user.length){
+            return res.status(409).json({
+                message:'mail already exists'
             })
+        }else{
+            brcypt.hash(req.body.password,10,(err,hash)=>{
+                if(err){
+                    return res.status(500).json({
+                        error:err
+                    })
+                }
+                else{
+                    const user=new User({
+                        _id:new mongoose.Types.ObjectId(),
+                        email:req.body.email,
+                        password:hash
+                    })
+                    user.save().then(result=>{
+                        console.log(result)
+                        res.status(201).json({
+                            message:'User created'
+                        })
+                    }).catch(err=>{
+                        console.log(err)
+                        res.status(500).json({
+                            error:err
+                        })
+                    })
+                }
+            }) 
         }
-        else{
-            const user=new User({
-                _id:new mongoose.Types.ObjectId(),
-                email:req.body.email,
-                password:hash
-            })
-            user.save().then(result=>{
-                console.log(result)
-                res.status(201).json({
-                    message:'User created'
-                })
-            }).catch(err=>{
-                console.log(err)
-                res.status(500).json({
-                    error:err
-                })
-            })
-        }
-    })
-    
+    })  
 })
-
+router.delete('/:userId',(req,res,next)=>{
+    User.findByIdAndDelete({_id:req.params.userId}).exec().then(result=>{
+        console.log(result)
+        res.status(200).json({
+            message:'User deleted'
+        })
+    }).catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    })
+})
 
 
 module.exports=router
