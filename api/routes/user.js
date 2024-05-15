83693edac72ea5301ebcd3dab99a0b5757a0ad18
vasanth-raja <vasathrajark@gmail.com>
2024-05-15 11:58:42 +0530

    JWT Token verification productauth

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router=express.Router();
 const mongoose= require('mongoose');
 const User=require("../models/user")
-const brcypt=require("bcrypt")
+const brcypt=require("bcrypt");
+const jwt=require('jsonwebtoken')
 
 router.post('/signup',(req,res,next)=>{
     User.find({email:req.body.email}).exec().then(user=>{
@@ -38,6 +39,44 @@ router.post('/signup',(req,res,next)=>{
             }) 
         }
     })  
+})
+router.post('/login',(req,res,next)=>{
+    User.find({email:req.body.email}).exec().then(user=>{
+        if(!user.length){
+            return res.status(401).json({
+                message:"Auth failed1"
+            })
+        }
+        brcypt.compare(req.body.password,user[0].password,(err,result)=>{
+            if(err){
+                return res.status(401).json({
+                    message:'Auth failed2'
+                })
+            }
+            if(result){
+                const token=jwt.sign({
+                    emial:user[0].email,
+                    userId:user[0]._id
+                },process.env.JWT_key,
+            {
+                expiresIn:"1h"
+            }
+        )
+                return res.status(200).json({
+                    message:'Auth successful',
+                    token:token
+                })
+            }
+            res.status(401).json({
+                message:'Auth failed3'
+            })
+        })
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
+    })
 })
 router.delete('/:userId',(req,res,next)=>{
     User.findByIdAndDelete({_id:req.params.userId}).exec().then(result=>{
